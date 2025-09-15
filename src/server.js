1616c934c4df509cc7a80f6e6b9d975e55a22ba0
Daniisalas875308 <875308@unizar.es>
2025-09-15 12:07:07 +0200

    Cambio cors

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 
 // habilita CORS solo para el frontend local
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5000",
+  origin: process.env.FRONTEND_URL || "http://localhost:3000",
   credentials: true
 }));
 
